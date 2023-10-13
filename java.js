cd11c647109ad10bcc43501d4a969aa22ce80976
Rikko <bulletbill51@yahoo.com>
2023-10-12 22:20:21 -0700

    Early Access 0.2 Added some Hololive EN members to game

--- java.js
+++ java.js
@@ -1,105 +1,181 @@
 const vtuberList = [
     { 
         Vtuber: "Enna",
+        Company: "Nijisanji",
         Wave: "Ethyria",
         Gender: "Female",
         hairColor: "White", 
     }, 
     {   Vtuber: "Millie", 
+        Company: "Nijisanji",
+
         Wave: "Ethyria",
         Gender: "Female",
         hairColor: "Blonde",
     },
     {
         Vtuber: "Nina",
+        Company: "Nijisanji",
         Wave: "Ethyria",
         Gender: "Female",
         hairColor: "Silver",
     },
     {
         Vtuber: "Pomu",
+        Company: "Nijisanji",
         Wave: "Lazulight",
         Gender: "Female",
         hairColor: "Blonde",
     },
     {
         Vtuber: "Elira",
+        Company: "Nijisanji",
         Wave: "Lazulight",
         Gender: "Female",
         hairColor: "Light Blue",
     },
     {
         Vtuber: "Selen",
+        Company: "Nijisanji",
         Wave: "Obsydia",
         Gender: "Female",
         hairColor: "Purple",
     },
     {
         Vtuber: "Finana",
+        Company: "Nijisanji",
         Wave: "Lazulight",
         Gender: "Female",
         hairColor: "Green",
     },
     {
         Vtuber: "Kotoka",
+        Company: "Nijisanji",
         Wave: "Xsoleil",
         Gender: "Female",
         hairColor: "Blonde",
     },
     {
         Vtuber: "Rosemi",
+        Company: "Nijisanji",
         Wave: "Obsydia",
         Gender: "Female",
         hairColor: "Pink",
     },
     {
         Vtuber: "Maria",
+        Company: "Nijisanji",
         Wave: "Iluna",
         Gender: "Female",
         hairColor: "Light Pink",
     },
     {
         Vtuber: "Reimu",
+        Company: "Nijisanji",
         Wave: "Ethyria",
         Gender: "Female",
         hairColor: "Silver",
     },
     {
         Vtuber: "Meloco",
+        Company: "Nijisanji",
         Wave: "Xsoleil",
         Gender: "Female",
         hairColor: "Black",
     },
     {
         Vtuber: "Petra",
+        Company: "Nijisanji",
         Wave: "Obsydia",
         Gender: "Female",
         hairColor: "Black",
     },
     {
         Vtuber: "Aia",
+        Company: "Nijisanji",
         Wave: "Iluna",
         Gender: "Female",
         hairColor: "Black",
     },
     {
         Vtuber: "Scarle",
+        Company: "Nijisanji",
         Wave: "Iluna",
         Gender: "Female",
         hairColor: "Black",
-    }
+    },
+    {
+        Vtuber: "Bijou",
+        Company: "Hololive",
+        Wave: "Advent",
+        Gender: "Female",
+        hairColor: "Purple"
+    },
+    {
+        Vtuber: "Nerissa",
+        Company: "Hololive",
+        Wave: "Advent",
+        Gender: "Female",
+        hairColor: "Black",
+    },
+    {
+        Vtuber: "Gura",
+        Company: "Hololive",
+        Wave: "Myth",
+        Gender: "Female",
+        hairColor: "White",
+    },
+    {
+        Vtuber: "Mumei",
+        Company: "Hololive",
+        Wave: "Promise",
+        Gender: "Female",
+        hairColor: "Brown",
+    },
+    {
+        Vtuber: "Kronii",
+        Company: "Hololive",
+        Wave: "Promise",
+        Gender: "Female",
+        hairColor: "Gray Blue",
+    },
+    {
+        Vtuber: "Ame",
+        Company: "Hololive",
+        Wave: "Myth",
+        Gender: "Female",
+        hairColor: "Blonde",
+    },
+    {
+        Vtuber: "Ina",
+        Company: "Hololive",
+        Wave: "Myth",
+        Gender: "Female",
+        hairColor: "Black",
+    },
+    {
+        Vtuber: "Baelz",
+        Company: "Hololive",
+        Wave: "Promise",
+        Gender: "Female",
+        hairColor: "Red",
+    },
+
 ]
 let randomVtuber = vtuberList[Math.floor(vtuberList.length * Math.random())];
 let actualAnswer = {
     Vtuber: randomVtuber.Vtuber.toLowerCase(),
     Wave: randomVtuber.Wave,
     Gender: randomVtuber.Gender,
-    hairColor: randomVtuber.hairColor
+    hairColor: randomVtuber.hairColor,
+    Company: randomVtuber.Company,
 
 }
 console.log(actualAnswer.Vtuber);
-let gameContainer= document.querySelector(".main-container2");
-let result= document.createElement("h1");
+
+let gameContainer = document.querySelector(".main-container2");
+let result = document.createElement("h1");
+
 function reveal(){
     let existingHints = document.querySelectorAll(".hint");
     if (existingHints.length > 0) {
@@ -145,9 +221,19 @@ function reveal(){
             else {
                 hint3.style.backgroundColor = "red";
             }
+            let hint4 = document.createElement ('div');
+            hint4.textContent = foundVtuber.Company;
+            hint4.classList.add("hint");
+            if (foundVtuber.Company === actualAnswer.Company) {
+                hint4.style.backgroundColor = "green";
+            }
+            else {
+                hint4.style.backgroundColor = "red";
+            }
             gameContainer.appendChild(hint1);
             gameContainer.appendChild(hint2);
             gameContainer.appendChild(hint3);
+            gameContainer.appendChild(hint4);
             
         }
         result.textContent = "Guess Again!";
