cc69f0258cc198af7a029d5e9425e0f2315e9f34
Rikko <bulletbill51@yahoo.com>
2023-10-13 17:35:31 -0700

    Early access 0.30 Added more vtubers! Added unseiso and boobaSize hints Moved resultsSection to above the hints resultsSection Now displays picture of actualAnswer when game is beat Fixed bug that allowed player to keep displaying hints when game over Changed hint textColor to white

--- java.js
+++ java.js
@@ -5,14 +5,18 @@ const vtuberList = [
         Picture: "https://yt3.googleusercontent.com/G0Que0jrJgUMYKpT_Ts8DU-eOWxpOZhqyRZphhw2_vfwrmrpKk5MGyEZChZ50JfrlCE9jfZoDw=s900-c-k-c0x00ffffff-no-rj",
         Wave: "Ethyria",
         Gender: "Female",
+        boobaSize: "Flat",
         hairColor: "Silver", 
+        Purity: "Unseiso",
     }, 
     {   Vtuber: "Millie", 
         Company: "Nijisanji",
         Picture: "https://pbs.twimg.com/media/FBGHzAtX0AUvtes.png",
         Wave: "Ethyria",
         Gender: "Female",
+        boobaSize: "Flat",
         hairColor: "Blonde",
+        Purity: "Unseiso",
     },
     {
         Vtuber: "Nina",
@@ -20,7 +24,9 @@ const vtuberList = [
         Picture: "https://yt3.googleusercontent.com/NjMA51STFyTrh_BoFeCBs5SbI73cwYTzBZqqVf3L2Nm8nzEel_GKx2IJCQ88mjZjbvHqehR_UA=s900-c-k-c0x00ffffff-no-rj",
         Wave: "Ethyria",
         Gender: "Female",
+        boobaSize: "Big",
         hairColor: "Silver",
+        Purity: "Unseiso",
     },
     {
         Vtuber: "Pomu",
@@ -28,7 +34,9 @@ const vtuberList = [
         Picture: "https://yt3.googleusercontent.com/nx24rX2DeH0FSJ96Oh1enaeAUiha-c_SN_m1Pci1vbR6ImMT6RV8j2n9RsAXRtFzH29cKJh_pQ=s900-c-k-c0x00ffffff-no-rj",
         Wave: "Lazulight",
         Gender: "Female",
+        boobaSize: "Flat",
         hairColor: "Blonde",
+        Purity: "Unseiso",
     },
     {
         Vtuber: "Elira",
@@ -36,7 +44,9 @@ const vtuberList = [
         Picture: "https://pbs.twimg.com/profile_images/1679346250519330817/oBQOoIQ7_400x400.jpg",
         Wave: "Lazulight",
         Gender: "Female",
+        boobaSize: "Big",
         hairColor: "Light Blue",
+        Purity: "Unseiso",
     },
     {
         Vtuber: "Selen",
@@ -44,7 +54,9 @@ const vtuberList = [
         Picture: "https://i.pinimg.com/564x/94/8d/2a/948d2ade766cb62e17eae9eecdb6fc7a.jpg",
         Wave: "Obsydia",
         Gender: "Female",
+        boobaSize: "Big",
         hairColor: "Purple",
+        Purity: "Normal",
     },
     {
         Vtuber: "Finana",
@@ -52,7 +64,9 @@ const vtuberList = [
         Picture: "https://yt3.googleusercontent.com/fL9tFSUSDCS-BkrZLrtAig3_55Tg0UaZxh2xChyUyoCElYh5tV05LOQWh68krLarWJ2unbLF=s900-c-k-c0x00ffffff-no-rj",
         Wave: "Lazulight",
         Gender: "Female",
+        boobaSize: "Flat",
         hairColor: "Green",
+        Purity: "Unseiso",
     },
     {
         Vtuber: "Kotoka",
@@ -60,7 +74,9 @@ const vtuberList = [
         Picture: "https://i1.sndcdn.com/artworks-Ge1mi53tV9pslVzk-ZmYlFA-t500x500.jpg",
         Wave: "Xsoleil",
         Gender: "Female",
+        boobaSize: "Big",
         hairColor: "Blonde",
+        Purity: "Unseiso",
     },
     {
         Vtuber: "Rosemi",
@@ -68,7 +84,9 @@ const vtuberList = [
         Picture: "https://images8.alphacoders.com/118/1183257.jpg",
         Wave: "Obsydia",
         Gender: "Female",
+        boobaSize: "Flat",
         hairColor: "Pink",
+        Purity: "Seiso",
     },
     {
         Vtuber: "Maria",
@@ -76,7 +94,9 @@ const vtuberList = [
         Picture: "https://i.pinimg.com/1200x/db/fe/09/dbfe09b1348bac1f66aa0c737b4a153c.jpg",
         Wave: "Iluna",
         Gender: "Female",
+        boobaSize: "Flat",
         hairColor: "Light Pink",
+        Purity: "Normal",
     },
     {
         Vtuber: "Reimu",
@@ -84,7 +104,9 @@ const vtuberList = [
         Picture: "https://nijisanji-store.com/cdn/shop/collections/yaGuFg4UHVClHs3Y_600x600_crop_center.jpg?v=1678274524",
         Wave: "Ethyria",
         Gender: "Female",
+        boobaSize: "Flat",
         hairColor: "Silver",
+        Purity: "Unseiso",
     },
     {
         Vtuber: "Meloco",
@@ -92,7 +114,9 @@ const vtuberList = [
         Picture: "https://pbs.twimg.com/profile_images/1642175100387721216/c4256QYI_400x400.jpg",
         Wave: "Xsoleil",
         Gender: "Female",
+        boobaSize: "Big",
         hairColor: "Black",
+        Purity: "Unseiso",
     },
     {
         Vtuber: "Petra",
@@ -100,7 +124,9 @@ const vtuberList = [
         Picture: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTsBLYLAw9IBijP5wNoKKU1Bm0UkYjdrXgIow&usqp=CAU",
         Wave: "Obsydia",
         Gender: "Female",
+        boobaSize: "Medium",
         hairColor: "Black",
+        Purity: "Normal",
     },
     {
         Vtuber: "Aia",
@@ -108,7 +134,9 @@ const vtuberList = [
         Picture: "https://static.zerochan.net/Aia.Amare.full.3720381.jpg",
         Wave: "Iluna",
         Gender: "Female",
+        boobaSize: "Medium",
         hairColor: "Black",
+        Purity: "Normal",
     },
     {
         Vtuber: "Scarle",
@@ -116,7 +144,9 @@ const vtuberList = [
         Picture: "https://cdn.donmai.us/original/a5/dc/a5dc077ab1fee039b399aef36f12c25b.jpg",
         Wave: "Iluna",
         Gender: "Female",
+        boobaSize: "Big",
         hairColor: "Black",
+        Purity: "Unseiso",
     },
     {
         Vtuber: "Bijou",
@@ -124,7 +154,9 @@ const vtuberList = [
         Picture: "https://i.pinimg.com/736x/a8/24/6d/a8246d182864d03fb4d6a2c017aed80d.jpg",
         Wave: "Advent",
         Gender: "Female",
-        hairColor: "Purple"
+        boobaSize: "Flat",
+        hairColor: "Purple",
+        Purity: "Unseiso",
     },
     {
         Vtuber: "Nerissa",
@@ -132,7 +164,9 @@ const vtuberList = [
         Picture: "https://static.miraheze.org/hololivewiki/3/3c/Nerissa_Ravencroft_-_Portrait_01.png",
         Wave: "Advent",
         Gender: "Female",
+        boobaSize: "Medium",
         hairColor: "Black",
+        Purity: "Normal",
     },
     {
         Vtuber: "Gura",
@@ -140,15 +174,19 @@ const vtuberList = [
         Picture: "https://i.pinimg.com/1200x/67/5c/88/675c889f62a5b3f21c9c83fb50d77c57.jpg",
         Wave: "Myth",
         Gender: "Female",
+        boobaSize: "Flat",
         hairColor: "White",
+        Purity: "Normal",
     },
     {
         Vtuber: "Mumei",
         Company: "Hololive",
-        Picture: "https://static.wikia.nocookie.net/youtube/images/7/79/Nanashi_Mumei_Ch._hololive-EN_3f.jpg/revision/latest?cb=20220109143904",
+        Picture: "https://i1.sndcdn.com/artworks-fJKzeLgbi1zHBOyz-JSsHfw-t500x500.jpg",
         Wave: "Promise",
         Gender: "Female",
+        boobaSize: "Flat",
         hairColor: "Brown",
+        Purity: "Normal",
     },
     {
         Vtuber: "Kronii",
@@ -156,7 +194,9 @@ const vtuberList = [
         Picture: "https://pbs.twimg.com/profile_images/1674511013713162240/COm2-Np7_400x400.jpg",
         Wave: "Promise",
         Gender: "Female",
+        boobaSize: "Big",
         hairColor: "Gray Blue",
+        Purity: "Normal",
     },
     {
         Vtuber: "Amelia",
@@ -164,7 +204,9 @@ const vtuberList = [
         Picture: "https://yt3.googleusercontent.com/RZ4Fp3L6_zyq6YA7s5WnH8-22iezMLwdJhtkBgs_EAb06mvMCnF59JKMNu9YPCqb7nhaeXMdPvY=s900-c-k-c0x00ffffff-no-rj",
         Wave: "Myth",
         Gender: "Female",
+        boobaSize: "Medium",
         hairColor: "Blonde",
+        Purity: "Unseiso",
     },
     {
         Vtuber: "Ina",
@@ -172,7 +214,9 @@ const vtuberList = [
         Picture: "https://avatarfiles.alphacoders.com/321/thumb-321791.jpg",
         Wave: "Myth",
         Gender: "Female",
+        boobaSize: "Flat",
         hairColor: "Black",
+        Purity: "Seiso",
     },
     {
         Vtuber: "Baelz",
@@ -180,17 +224,67 @@ const vtuberList = [
         Picture: "https://pbs.twimg.com/profile_images/1648675109463547908/MO3l8PKv_400x400.jpg",
         Wave: "Promise",
         Gender: "Female",
+        boobaSize: "Medium",
         hairColor: "Red",
+        Purity: "Unseiso",
     },
+    {
+        Vtuber: "Fauna",
+        Company: "Hololive",
+        Picture: "https://pbs.twimg.com/profile_images/1642973463827849216/pq3uFh8Z_400x400.jpg",
+        Wave: "Promise",
+        Gender: "Female",
+        boobaSize: "Big",
+        hairColor: "Green",
+        Purity: "Seiso",
+    },
+    {
+        Vtuber: "Irys",
+        Company: "Hololive",
+        Picture: "https://pbs.twimg.com/profile_images/1676323483540914177/MlnEvgwN_400x400.jpg",
+        Wave: "Promise",
+        Gender: "Female",
+        boobaSize: "Medium",
+        hairColor: "Red",
+        Purity: "Unseiso",
+    },
+    {
+        Vtuber: "Kiara",
+        Company: "Hololive",
+        Picture: "https://pbs.twimg.com/media/FDYeg9RXEAUEuEA.png",
+        Wave: "Myth",
+        Gender: "Female",
+        boobaSize: "Big",
+        hairColor: "Orange",
+        Purity: "Unseiso",
+    },
+    {
+        Vtuber: "Calliope",
+        Company: "Hololive",
+        Picture: "https://i.pinimg.com/1200x/52/41/f7/5241f78deaf283ea72f32e66d7522a23.jpg",
+        Wave: "Myth",
+        Gender: "Female",
+        boobaSize: "Big",
+        hairColor: "Pink",
+        Purity: "Unseiso",
+    },
+    
+    
+
+    
+    
 
 ]
 let randomVtuber = vtuberList[Math.floor(vtuberList.length * Math.random())];
 let actualAnswer = {
     Vtuber: randomVtuber.Vtuber.toLowerCase(),
+    Picture: randomVtuber.Picture,
     Wave: randomVtuber.Wave,
     Gender: randomVtuber.Gender,
     hairColor: randomVtuber.hairColor,
     Company: randomVtuber.Company,
+    boobaSize: randomVtuber.boobaSize,
+    Purity: randomVtuber.Purity,
 
 }
 console.log(actualAnswer.Vtuber);
@@ -198,7 +292,7 @@ console.log(actualAnswer.Vtuber);
 let resultsSection = document.querySelector("#results");
 let gameContainer = document.querySelector("#gameContainer");
 let result = document.createElement("h1");
-
+let gameOver = false;
 function reveal(){
    /* let existingHints = document.querySelectorAll(".hint");
     if (existingHints.length > 0) {
@@ -207,11 +301,20 @@ function reveal(){
         }
     }
     */
+   if (gameOver === true) {
+    return;
+   }
+   else {
     let playerAnswer = document.getElementById("answer").value;
     console.log(playerAnswer);
     if (playerAnswer.toLowerCase() === actualAnswer.Vtuber) {
         result.textContent = "You Win!";
+        let victoryPic = document.createElement('img');
+        victoryPic.classList.add("victoryPic");
+        victoryPic.src = actualAnswer.Picture;
         resultsSection.appendChild(result);
+        resultsSection.appendChild(victoryPic);
+        gameOver = true;
         } 
     else {  
         const foundVtuber = vtuberList.find(vtuber => vtuber.Vtuber.toLowerCase() === playerAnswer.toLowerCase());
@@ -264,13 +367,35 @@ function reveal(){
             else {
                 hint5.style.backgroundColor = "red";
             }
+
+            let hint6 = document.createElement ('div');
+            hint6.textContent = foundVtuber.boobaSize;
+            hint6.classList.add("hint");
+            if (foundVtuber.boobaSize === actualAnswer.boobaSize) {
+                hint6.style.backgroundColor = "green";
+            }
+            else {
+                hint6.style.backgroundColor = "red";
+            }
+
+            let hint7 = document.createElement ('div');
+            hint7.textContent = foundVtuber.Purity;
+            hint7.classList.add("hint");
+            if (foundVtuber.Purity === actualAnswer.Purity) {
+                hint7.style.backgroundColor = "green";
+            }
+            else {
+                hint7.style.backgroundColor = "red";
+            }
+
             let hintsSection = document.createElement('div');
             hintsSection.classList.add("hintsSection");
             hintsSection.appendChild(hint1);
             hintsSection.appendChild(hint5);
             hintsSection.appendChild(hint2);
             hintsSection.appendChild(hint3);
-            hintsSection.appendChild(hint4);
+            hintsSection.appendChild(hint6);
+            hintsSection.appendChild(hint7);
             hintsSection.appendChild(hint4);
             gameContainer.insertBefore(hintsSection, gameContainer.firstChild)
             
@@ -278,6 +403,7 @@ function reveal(){
         result.textContent = "Guess Again!";
         resultsSection.appendChild(result);
     }
+}
 }
 
 
